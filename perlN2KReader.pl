5421673638d8c8cc8954d15081eee33deca1b5e9
Robert <rkrasowski@yahoo.com>
2013-11-16 10:22:42 -0600

    Reader in perl works

--- perlN2KReader.pl
+++ perlN2KReader.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 
-open(PS,"./actisense-serial -r /dev/ttyO2 | ./analyzer |") || die "Failed: $!\n";
+open(PS,"./linux-arm-gnuaebi/actisense-serial -r /dev/ttyO2 | ./linux-arm-gnuaebi/analyzer |") || die "Failed: $!\n";
 while ( <PS> )
 
 	{
