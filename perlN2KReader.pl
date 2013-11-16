442ecd3a993a72dfc1e5c645ca461d566daae16f
Robert <rkrasowski@yahoo.com>
2013-11-16 11:25:06 -0600

    Date and Time works

--- perlN2KReader.pl
+++ perlN2KReader.pl
@@ -3,16 +3,84 @@ use strict;
 use warnings;
 
 
-open(PS,"./linux-arm-gnuaebi/actisense-serial -r /dev/ttyO2 | ./linux-arm-gnuaebi/analyzer |") || die "Failed: $!\n";
+open(PS,"./linux-arm-gnuaebi/actisense-serial -r /dev/ttyO2 | ./linux-arm-gnuaebi/analyzer -json|") || die "Failed: $!\n";
 while ( <PS> )
 
 	{
+		my $time = systemTime($_);
+		if ($time)
+			{
+				print "Time is: $time\n";
+			}
+			
+				
+		
 
-		print "$_";
-
+		my $date = systemDate($_);
+		if ($date)
+			{		
+				print "Date is :$date\n";
+			}
+		#if ($_ =~ m/COG/)
+		#	{
+	#			print "$_";
+		#	}
 	}
 
 close(PS);
+
+###################################################################################################
+
+
+sub systemTime 
+	{
+		my $data = shift;
+		if ($data =~ m/pgn\":\"126992/)
+			{
+
+				my @splitData = split (/,/,$data);
+				foreach(@splitData)
+					{
+						if ($_ =~ m/\"Time/g)
+							{
+								my @timeSplit = split(/\"/,$_);
+								my $timeSplit;
+								return $timeSplit[3];
+							}
+
+					}
+
+
+			}
+	}
+
+sub systemDate 
+	{
+
+
+	        my $data = shift;
+                if ($data =~ m/pgn\":\"126992/)
+                        {
+
+                                my @splitData = split (/,/,$data);
+                                foreach(@splitData)
+                                        {
+
+                                                if ($_ =~m/\"Time/g)
+                                                        {
+                                                                my @dateSplit = split(/\"/,$_);
+                                                                my $dateSplit;
+                                                                print "Date from sub $dateSplit[3]\n";
+								return $dateSplit[3];
+                                                        }
+                                        }
+
+
+                        }
+        }
+
+
+
 
 
 
