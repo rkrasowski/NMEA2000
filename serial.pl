e53c3c1512ab9ac4cf3372bfc9f93b8043efa532
Robert <rkrasowski@yahoo.com>
2013-11-15 21:39:51 -0600

    Serial port working on UART2 and tested up to 115200

--- serial.pl
+++ serial.pl
@@ -5,12 +5,26 @@ use warnings;
 use Device::SerialPort;
 
 
-my $PORT = "/dev/ttyO1"; 
+my $PORT = "/dev/ttyO2"; 
 my $serialData;
+
+
+
+# set pins in appropriate modes to communicate with  modem:
+# Set UART 2
+# Set Tx pin
+`echo 1 > /sys/kernel/debug/omap_mux/spi0_d0`;
+# set Rx pin
+`echo 21 > /sys/kernel/debug/omap_mux/spi0_sclk`;
+
+
+
+
+
 
 my $ob = Device::SerialPort->new($PORT) || die "Can't Open $PORT: $!";
 
-$ob->baudrate(9600) || die "failed setting baudrate";
+$ob->baudrate(115200) || die "failed setting baudrate";
 $ob->parity("none") || die "failed setting parity";
 $ob->databits(8) || die "failed setting databits";
 #$ob->stty_icrnl(1) || die "failed setting convert cr to new line";
@@ -23,11 +37,14 @@ $| = 1;
 open( DEV, "<$PORT" ) || die "Cannot open $PORT: $_";
 
 
-
+my $i = 1;
 while (1)
 	{
-		$ob->write("Testing serial connection\n");
+		print"Transmiting test number $i\n";
+		$ob->write("Testing serial connection number $i\n");
+		#select(undef,undef,undef,0.25);
 		sleep(1);
+		$i = $i + 1;
 	}
 
 
